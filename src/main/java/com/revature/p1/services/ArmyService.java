3e2f95c5281991f4a933246eaba0773600be6e88
Ruben <rframirez1993@gmail.com>
2023-06-12 13:21:59 -0700

    View users and now able to view armies by user

--- src/main/java/com/revature/p1/services/ArmyService.java
+++ src/main/java/com/revature/p1/services/ArmyService.java
@@ -1,12 +1,17 @@
 package com.revature.p1.services;
 
+import com.revature.p1.dtos.requests.FindUserRequest;
 import com.revature.p1.dtos.requests.NewArmyRequest;
 import com.revature.p1.dtos.requests.NewRoleRequest;
 import com.revature.p1.entities.Army;
 import com.revature.p1.entities.Role;
+import com.revature.p1.entities.User;
 import com.revature.p1.repositories.ArmyRepo;
+import com.revature.p1.utils.UserNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -17,4 +22,8 @@ public class ArmyService {
         Army newArmy = new Army(username);
         return armyRepo.save(newArmy);
     }
+
+    public Optional<Army> findByUsername(String username) {
+        return armyRepo.findByName(username);
+    };
 }
